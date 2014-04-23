f5b1923faf463e0b9de1de286d73e986b87f3147
Mike Davis <michaeltdavis@gmail.com>
2014-04-22 22:34:43 -0400

    merge lights into grid; snipe hit detection

--- app/scripts/services/gameservice.js
+++ app/scripts/services/gameservice.js
@@ -5,10 +5,17 @@ angular.module('snipehuntApp')
     {
         this.startNewGame = function(height, width, snipes)
         {
+            this.gridHeight = height;
+            this.gridWidth = width;
             this.snipesVisible = false;
 
             //create the grid.
             this.grid = [];
+
+            var northRow = 0;
+            var southRow = height - 1;
+            var westCol = 0;
+            var eastCol = width - 1;
 
             for(var rowNum = 0; rowNum < height; rowNum++)
             {
@@ -16,56 +23,126 @@ angular.module('snipehuntApp')
 
                 for(var colNum = 0; colNum < width; colNum++)
                 {
-                    row.push({
-                        snipe:false,
-                        cage:false,
-                        mark:false
-                    });
+                    if(westCol < colNum && colNum < eastCol)
+                    {
+                        if(rowNum === northRow || rowNum === southRow)
+                        {
+                            row.push(this.makeLight());
+                        }
+                        else
+                        {
+                            row.push({
+                                snipe:false,
+                                cage:false,
+                                mark:false
+                            });
+                        }
+                    }
+                    else if(northRow < rowNum && rowNum < southRow)
+                    {
+                        row.push(this.makeLight());
+                    }
+                    else
+                    {
+                        row.push({});
+                    }
                 }
 
                 this.grid.push(row);
             }
 
-            //create the lights.
-            this.lights = {};
-            this.clearLights("north", width);
-            this.clearLights("east", height);
-            this.clearLights("south", width);
-            this.clearLights("west", height);
-
             //assign the snipes to random locations in the grid.
             var assignedSnipes = 0;
             while(assignedSnipes < snipes)
             {
-                var row = Math.floor(Math.random() * height);
-                var colNum = Math.floor(Math.random() * width);
+                var rowNum = Math.floor(Math.random() * (height - 2)) + 1;
+                var colNum = Math.floor(Math.random() * (width - 2)) + 1;
 
-                if(!this.grid[row][colNum].snipe)
+                if(!this.grid[rowNum][colNum].snipe)
                 {
-                    this.grid[row][colNum].snipe = true;
+                    this.grid[rowNum][colNum].snipe = true;
                     assignedSnipes++;
                 }
             }
         };
 
-        this.clearLights = function(side, numLights)
+        this.makeLight = function()
         {
-            this.lights[side] = [];
+            return {
+                used:false,
+                hit:false,
+                reflection:false,
+                passedThrough:false,
+                linkId:0
+            };
+        };
 
-            for(var i = 0; i < numLights; i++)
+        this.turnOnLight = function(rowNum, colNum)
+        {
+            var light = this.grid[rowNum][colNum];
+            if(!light.used)
             {
-                this.lights[side].push({
-                    hit:false,
-                    reflection:false,
-                    passedThrough:false,
-                    linkId:0
-                });
+                light.used = true;
+
+                var beam = {};
+
+                beam.sourceLight = light;
+                beam.rowNum = rowNum;
+                beam.colNum = colNum;
+
+                if(rowNum === 0)
+                {
+                    //north side.
+                    beam.horizontalDirection = 0;
+                    beam.verticalDirection = 1;
+                }
+                else if(colNum === this.gridWidth - 1)
+                {
+                    //east side.
+                    beam.horizontalDirection = -1;
+                    beam.verticalDirection = 0;
+                }
+                else if(rowNum === this.gridHeight - 1)
+                {
+                    //south side.
+                    beam.horizontalDirection = 0;
+                    beam.verticalDirection = -1;
+                }
+                else if(colNum === 0)
+                {
+                    //west side.
+                    beam.horizontalDirection = 1;
+                    beam.verticalDirection = 0;
+                }
+
+                this.resolveBeam(beam);
             }
         };
 
-        this.turnOnLight = function(side, colNum)
+        this.resolveBeam = function(beam)
         {
+            while(true)
+            {
+                try
+                {
+                    var nextCell = this.grid[beam.rowNum + beam.verticalDirection][beam.colNum + beam.horizontalDirection];
 
+                    if(nextCell.snipe)
+                    {
+                        beam.sourceLight.hit = true;
+                        break;
+                    }
+                    else
+                    {
+                        beam.rowNum += beam.verticalDirection;
+                        beam.colNum += beam.horizontalDirection;
+                    }
+                }
+                catch(ex)
+                {
+                    break;
+                }
+            }
         };
 
         this.toggleCage = function(rowNum, colNum)
@@ -81,10 +158,5 @@ angular.module('snipehuntApp')
         this.revealSnipes = function()
         {
             this.snipesVisible = true;
-        };
-
-        this.getLights = function(side)
-        {
-
         };
     });
--- test/spec/services/gameservice.js
+++ test/spec/services/gameservice.js
@@ -26,12 +26,12 @@ describe('Service: GameService', function()
         expect(GameService.grid).toBeDefined();
         expect(GameService.grid.length).toBe(height);
 
-        for(var rowNum = 0; rowNum < height; rowNum++)
+        for(var rowNum = 1; rowNum < height - 1; rowNum++)
         {
             expect(GameService.grid[rowNum]).toBeDefined();
             expect(GameService.grid[rowNum].length).toBe(width);
 
-            for(var colNum = 0; colNum < width; colNum++)
+            for(var colNum = 1; colNum < width - 1; colNum++)
             {
                 expect(GameService.grid[rowNum][colNum]).toBeDefined();
 
@@ -44,13 +44,9 @@ describe('Service: GameService', function()
 
     it("should create the required lamps", function()
     {
-        spyOn(GameService, 'clearLights').andCallThrough();
-
         var height = 10;
         var width = 5;
         GameService.startNewGame(height, width, 1);
-
-        expect(GameService.clearLights.callCount).toBe(4);
 
         function checkLight(light)
         {
@@ -61,62 +57,51 @@ describe('Service: GameService', function()
             expect(light.linkId).toBeDefined();
         }
 
-        for(var colNum = 0; colNum < width; colNum++)
+        for(var colNum = 1; colNum < width - 1; colNum++)
         {
-            checkLight(GameService.lights.north[colNum]);
-            checkLight(GameService.lights.south[colNum]);
+            checkLight(GameService.grid[0][colNum]);
+            checkLight(GameService.grid[height - 1][colNum]);
         }
 
-        for(var rowNum = 0; rowNum < height; rowNum++)
+        for(var rowNum = 1; rowNum < height - 1; rowNum++)
         {
-            checkLight(GameService.lights.east[rowNum]);
-            checkLight(GameService.lights.west[rowNum]);
+            checkLight(GameService.grid[rowNum][0]);
+            checkLight(GameService.grid[rowNum][width - 1]);
         }
     });
 
     it("should assign the required number of snipes", function()
     {
-        var height = 10;
-        var width = 10;
+        var height = 12;
+        var width = 12;
+
+        function countSnipes()
+        {
+            var assignedSnipes = 0;
+            for(var rowNum = 0; rowNum < height; rowNum++)
+            {
+                for(var colNum = 0; colNum < height; colNum++)
+                {
+                    if(GameService.grid[rowNum][colNum].snipe)
+                    {
+                        assignedSnipes++;
+                    }
+                }
+            }
+            return assignedSnipes;
+        }
 
         //empty grid.
         GameService.startNewGame(height, width, 0);
-
-        for(var rowNum = 0; rowNum < height; rowNum++)
-        {
-            for(var colNum = 0; colNum < height; colNum++)
-            {
-                expect(GameService.grid[rowNum][colNum].snipe).toBe(false);
-            }
-        }
+        expect(countSnipes()).toBe(0);
 
         //half-filled grid.
-        GameService.startNewGame(height, width, height * width / 2);
-        var assignedSnipes = 0;
-
-        for(var rowNum = 0; rowNum < height; rowNum++)
-        {
-            for(var colNum = 0; colNum < height; colNum++)
-            {
-                if(GameService.grid[rowNum][colNum].snipe)
-                {
-                    assignedSnipes++;
-                }
-            }
-        }
-
-        expect(assignedSnipes).toBe(height * width / 2);
+        GameService.startNewGame(height, width, 50);
+        expect(countSnipes()).toBe(50);
 
         //full grid.
-        GameService.startNewGame(height, width, height * width);
-
-        for(var rowNum = 0; rowNum < height; rowNum++)
-        {
-            for(var colNum = 0; colNum < height; colNum++)
-            {
-                expect(GameService.grid[rowNum][colNum].snipe).toBe(true);
-            }
-        }
+        GameService.startNewGame(height, width, 100);
+        expect(countSnipes()).toBe(100);
     });
 
     it("should toggle a cage when told to", function()
@@ -158,5 +143,84 @@ describe('Service: GameService', function()
         GameService.revealSnipes();
 
         expect(GameService.snipesVisible).toBe(true);
+    });
+
+    it("should turn on a light when told to", function()
+    {
+        GameService.startNewGame(5, 5, 4);
+
+        expect(GameService.grid[0][2].used).toBe(false);
+
+        GameService.turnOnLight(0, 2)
+
+        expect(GameService.grid[0][2].used).toBe(true);
+    });
+
+    it("should create a beam correctly", function()
+    {
+        GameService.startNewGame(5, 5, 0);
+
+        spyOn(GameService, "resolveBeam");
+
+        GameService.turnOnLight(0, 2);
+
+        expect(GameService.resolveBeam).toHaveBeenCalledWith({
+            sourceLight:{
+                used:true,
+                hit:false,
+                reflection:false,
+                passedThrough:false,
+                linkId:0
+            },
+            horizontalDirection:0,
+            verticalDirection:1,
+            rowNum: 0,
+            colNum: 2
+        });
+
+        GameService.turnOnLight(1, 0);
+
+        expect(GameService.resolveBeam).toHaveBeenCalledWith({
+            sourceLight:{
+                used:true,
+                hit:false,
+                reflection:false,
+                passedThrough:false,
+                linkId:0
+            },
+            horizontalDirection:1,
+            verticalDirection:0,
+            rowNum: 1,
+            colNum: 0
+        });
+    });
+
+    it("should register a snipe hit from all four directions", function()
+    {
+        //assign a snipe manually.
+        var width = 5;
+        var height = 4;
+        var snipeRow = 1;
+        var snipeCol = 2;
+        GameService.startNewGame(height, width, 0);
+        GameService.grid[snipeRow][snipeCol].snipe = true;
+
+        for(var colNum = 1; colNum < width - 1; colNum++)
+        {
+            GameService.turnOnLight(0, colNum);
+            expect(GameService.grid[0][colNum].hit).toBe(colNum === snipeCol);
+
+            GameService.turnOnLight(height - 1, colNum);
+            expect(GameService.grid[height - 1][colNum].hit).toBe(colNum === snipeCol);
+        }
+
+        for(var rowNum = 1; rowNum < height - 1; rowNum++)
+        {
+            GameService.turnOnLight(rowNum, width - 1);
+            expect(GameService.grid[rowNum][width - 1].hit).toBe(rowNum === snipeRow);
+
+            GameService.turnOnLight(rowNum, 0);
+            expect(GameService.grid[rowNum][0].hit).toBe(rowNum === snipeRow);
+        }
     });
 });
